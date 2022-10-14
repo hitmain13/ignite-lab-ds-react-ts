7fea26e4b381daf261d6273b6b9951c0cacc96df
Fabio Matsumoto <fabio.matsumoto@br.experian.com>
2022-10-14 08:33:51 -0300

    fix: basePath and package-lock

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -15,7 +15,7 @@ module.exports = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === "PRODUCTION") {
-      config.base = "/ignite-lab-design-system/";
+      config.base = "/ignite-lab-ds-react-ts/";
     }
 
     return config;
